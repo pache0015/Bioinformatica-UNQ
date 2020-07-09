7f7eda2d8b174660829a904beb2ae21c4712b765
Mariano <mariano_a_p@hotmail.com>
2020-07-09 17:02:42 -0300

    Alineacion completa, y correccion del arbol

--- TP - FINAL/clases/Alineador.py
+++ TP - FINAL/clases/Alineador.py
@@ -1,4 +1,22 @@
-class Mapa:
-    pass
-    
+import os
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+import Bio.SeqIO as SeqIO
+from Bio.Align.Applications import ClustalOmegaCommandline
 
+class Alineador:
+    def alinear(self):
+        if os.path.exists("./temp"):
+            owd = os.getcwd()
+            os.chdir("./temp")
+        else:
+            print("ERROR - No se puede cambiar la ruta de destino")    
+
+        in_file = "fasta.fasta"
+        out_file = "aligned.fasta"
+
+        clustalomega_cline = ClustalOmegaCommandline(infile=in_file, outfile=out_file, verbose=True, auto=True, force=True)
+        clustalomega_cline()
+
+        os.chdir(owd)
--- TP - FINAL/clases/Arbol.py
+++ TP - FINAL/clases/Arbol.py
@@ -1,9 +1,10 @@
-from ete3 import PhyloTree, faces, TreeStyle, NodeStyle
+from ete3 import PhyloTree, faces, TreeStyle, NodeStyle, AttrFace, TextFace
 import subprocess
+import tkinter as tk
 
 class Arbol:
     def __init__(self):
-        test = subprocess.Popen(["iqtree","-s","./temp/fasta.fasta"], stdout=subprocess.PIPE)
+        test = subprocess.Popen(["iqtree","-s","./temp/fasta.fasta", "-st", "AA", "-m", "BLOSUM62"], stdout=subprocess.PIPE)
         test.communicate()[0]
         self.alg = open("./temp/fasta.fasta", "r").read()
         self.newick = open("./temp/fasta.fasta.treefile", "r").read()
@@ -15,11 +16,20 @@ class Arbol:
         genetree.link_to_alignment(self.alg)
         return genetree, TreeStyle()
 
+    def treeLayout(self, node):
+        if node.is_leaf():
+            faces.add_face_to_node(AttrFace("name"), node, column=0)
+        else:
+            node.img_style["size"] = 4
+            node.img_style["shape"] = "sphere"
+            node.img_style["fgcolor"] = "#AA0000"
+
     def armarArbol(self):
         # Visualize the reconciled tree
         t, ts = self.getPhyloTree()
-        ts.scale = 20
-        ts.show_leaf_name = True
-        ts.show_branch_length = True
+        ts.scale = 200
+        ts.show_leaf_name = False
         ts.show_branch_support = True
+        ts.show_branch_length = True
+        ts.layout_fn = self.treeLayout
         t.show(tree_style=ts)
